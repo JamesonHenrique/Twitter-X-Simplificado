d487ef8c7e4b1f95b654a891ad7cbbd04df241db
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-05-10 02:36:39 -0300

    Gerar a chave publica e privada do token JWT

--- twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/config/SecurityConfig.java
+++ twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/config/SecurityConfig.java
@@ -1,16 +1,34 @@
 package twitterx.simplificado.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import static org.springframework.security.oauth2.jwt.NimbusJwtEncoder.*;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
     @Bean
     private SecurityFilterChain securityFilterChain(
             HttpSecurity http) throws Exception{
@@ -24,4 +42,15 @@ public class SecurityConfig {
         ;
                 return http.build();
     }
-}
+    @Bean
+    private JwtEncoder jwtEncoder(){
+        JWK jwk = new RSAKey.Builder(publicKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+    @Bean
+    private JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }}
+
+
