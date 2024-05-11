3c8becbd13ec0c9cf1af056c6de4d561e74796b0
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-05-11 01:57:17 -0300

    Corrigir erros

--- twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/config/SecurityConfig.java
+++ twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/config/SecurityConfig.java
@@ -7,7 +7,9 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,40 +26,47 @@ import java.security.interfaces.RSAPublicKey;
 
 import static org.springframework.security.oauth2.jwt.NimbusJwtEncoder.*;
 
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-    @Value("${jwt.private.key}")
-    private RSAPrivateKey privateKey;
+
     @Value("${jwt.public.key}")
     private RSAPublicKey publicKey;
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
+
     @Bean
-    private SecurityFilterChain securityFilterChain(
-            HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .authorizeRequests( authorize -> authorize.anyRequest().authenticated())
-                .csrf( (csrf) -> csrf.disable())
-                .oauth2ResourceServer( (oauth2) -> oauth2.jwt(
-                        Customizer.withDefaults()))
-                .sessionManagement( (session) -> session.sessionCreationPolicy(
-                        SessionCreationPolicy.STATELESS))
-        ;
-                return http.build();
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
     }
+
     @Bean
-    private JwtEncoder jwtEncoder(){
-        JWK jwk = new RSAKey.Builder(publicKey).build();
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
         var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
         return new NimbusJwtEncoder(jwks);
     }
+
     @Bean
-    private JwtDecoder jwtDecoder(){
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
-    }
-    @Bean
-    private BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
-
-
--- twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/controller/TokenController.java
+++ twitter_x-simplificado/twitter-x-simplificado/src/main/java/twitterx/simplificado/controller/TokenController.java
@@ -12,54 +12,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import twitterx.simplificado.controller.dto.LoginRequest;
 import twitterx.simplificado.controller.dto.LoginResponse;
+import twitterx.simplificado.entities.Role;
 import twitterx.simplificado.repository.UserRepository;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
-    public TokenController(
-            UserRepository userRepository,
-            BCryptPasswordEncoder bCryptPasswordEncoder,
-            JwtEncoder jwtEncoder) {
-        this.userRepository =
-                userRepository;
-        this.bCryptPasswordEncoder =
-                bCryptPasswordEncoder;
-        this.jwtEncoder =
-                jwtEncoder;
+
+    private final JwtEncoder jwtEncoder;
+    private final UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public TokenController(JwtEncoder jwtEncoder,
+                           UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.jwtEncoder = jwtEncoder;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final JwtEncoder
-            jwtEncoder;
-    public TokenController(JwtEncoder jwtEncoder){
-        this.jwtEncoder = jwtEncoder;
-    }
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        var user = userRepository.findByUsername(loginRequest.username());
-  if(user.isEmpty()  || !user.get().isLoginCorrect(loginRequest, bCryptPasswordEncoder)){
-      throw new BadCredentialsException("Invalid username or password");
-  }
-  var now =
-          Instant.now();
-  var expiresIn = 300L;
-  var claims =
-          JwtClaimsSet.builder()
-                  .issuer("backend")
-                  .subject(user.get().getUserId().toString())
-                  .subject(user.get().getUsername())
-                  .expiresAt(now.plusSeconds(expiresIn))
 
-                  .build();
-        var jwtValue = jwtEncoder.encode(
-                JwtEncoderParameters.from(claims)).getTokenValue();
+        var user = userRepository.findByUsername(loginRequest.username());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
+            throw new BadCredentialsException("user or password is invalid!");
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+
+        var scopes = user.get().getRoles()
+                .stream()
+                .map(Role::getName)
+                .collect(
+                        Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("mybackend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
         return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
     }
-
-
 }
